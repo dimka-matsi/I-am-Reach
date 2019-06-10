46e257429dd6919e7491d8de91150989519dfa9b
Dmitry Matskello <dimka_matsi@MacBook-Pro-Dmitry.local>
2019-06-10 10:41:53 +0300

    app I am Rich

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dmitry Matskello on 6/7/19.
+//  Copyright © 2019 Dmitry Matskello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
